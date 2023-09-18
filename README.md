49b5159d7d88c958470a51c35d2053116072a811
Hamoon1987 <hamoon.jafarian@ontariotechu.net>
2023-09-18 14:58:45 +0000

    updated readme

--- README.md
+++ README.md
@@ -8,27 +8,30 @@ You need a linux server with gpu. Install Docker, NVIDIA Container Toolkit, VSCo
 3- Run the docker image: ```docker run -it -d --gpus all --name my_anomalib anomalib```  
 4- Attach to the running container  
   
-## Default options
-1- We need normal and abnormal images of the product to train the model. Put the normal, abnormal, and the images you want to check afterwards in ```datasets/custom/normal```, ```datasets/custom/abnormal```, and ```datasets/custom/check``` folders respectively    
-2- To trian the model: ```python tools/train.py```  
-3- To check the new images: ```python tools/inference/lightning_inference.py```  
-Check the results in ```results/patchcore/custom/checkimages```  
-4- To evaluate the model: ```python tools/test.py```  
-## Basic options
+## Training
 1- Put the normal, abnormal, and the images you want to check afterwards in ```datasets/<product_name>/normal```, ```datasets/<product_name>/abnormal```, and ```datasets/<product_name>/check``` folders respectively   
 Example: ```datasets/cable/normal```, ```datasets/cable/abnormal```, and ```datasets/cable/check```  
+If abnormal images are not available you can just put some normal images inside (evaluation option won't be available). Also, you can add inference images to the check folder later or you can upload the files from local machine during the inference time  
 2- Set the training configuration: ```python changeConfig.py --product <product_name> --path ./datasets/<product_name> --model <model_name>  --tag <tag_name>```  
 Example: ```python changeConfig.py --product cable --path ./datasets/cable --model patchcore  --tag patchcore_cable_v01```  
 3- To trian the model: ```python tools/train.py --tag <tag>```  
 Example: ```python tools/train.py --tag patchcore_cable_v01```  
-4- To check the new images: ```python tools/inference/lightning_inference.py --tag <tag_name>```  
-Example: ```python tools/inference/lightning_inference.py --tag patchcore_cable_v01```  
-Check the results in ```results/patchcore/custom/checkimages```  
-5- To evaluate the model: ```python tools/test.py --tag <tag_name>``` 
+4- To evaluate the model: ```python tools/test.py --tag <tag_name>``` 
 Example:  ```python tools/test.py --tag patchcore_cable_v01```  
-## Advance options
-1- Set the training configuration: ```python changeConfig.py  --model <model_name>  --tag <tag_name>```  
-Example: ```python changeConfig.py --model patchcore  --tag patchcore_cable_v01```  
-2- Open the config file ```my_configs/<tag_name>.yaml```, browse and change the advance options  
-3- The rest of the process is as before, you can use the ```<tag_name>``` to train, test, and evaluate the model  
+
+## Inference
+1- After training the models, run the app: ```python app.py```  
+2- Choose the model tag from the dropdown menu  
+<p align="center">
+	<img width="450" height="300" src="media/first.jpg">
+</p>
+3- Set the path to the inference images on the server or choose the images from your local machine   
+<p align="center">
+	<img width="450" height="300" src="media/second.jpg">
+</p>
+4- The result will be shown afterwards:  
+<p align="center">
+	<img width="450" height="300" src="media/third.jpg">
+</p>
+
 
--- app.py
+++ app.py
@@ -1,43 +1,129 @@
 # app.py
 import subprocess
-from flask import Flask, render_template, request, send_from_directory, send_file
+from flask import Flask, render_template, request, send_from_directory, send_file, session
 import os
-
+import shutil
+import glob
 
 app = Flask(__name__)
+app.secret_key = '184174562bf2be3fc810beeaa41ac1140d3ecc3565d5269f'
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+
+@app.route('/')
+def home():
     dropdown_options = os.listdir("my_configs/")
     dropdown_options.remove('patchcore_custom.yaml')
     dropdown_options = [i[:-5] for i in dropdown_options]
-    # dropdown_options = ['patchCore_cable', 'patchCore_bottle', 'patchCore_oilCap']
 
+    return render_template('home.html', dropdown_options = dropdown_options)
+
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
     if request.method == 'POST':
-        print("hello")
-        selected_option = request.form['dropdown']
-        path = execute_command(selected_option)
-        images = os.listdir(path)
-        return render_template('index.html', dropdown_options=dropdown_options, path = path, images=images)
-    
-    return render_template('index.html', dropdown_options=dropdown_options)
+        tag = request.form['dropdown']
+        path = get_path(tag)
+        product = get_product(tag)
+        session['tag'] = tag
+        session['product'] = product
+    return render_template('secondPage.html', tag=tag, path = path)
 
-def execute_command(option):
-    # Build the command string
-    command = f"python tools/inference/lightning_inference.py --tag {option}"
+    
+
+@app.route('/submit_address', methods=['POST'])
+def submit_address():
+    # if 'submit' in request.form:
+    address = request.form['address']
+    tag = session.get('tag')
+    product = session.get('product')
+    # Do something with the submitted address, e.g., process it or save it
+    change_address(product, address, tag)
+    inference(tag)
+    results_path = get_results(product, address)
+    images = os.listdir(results_path)
+    
+    return render_template('thirdPage.html', address = address, images=images, tag=tag, results_path=results_path)
+
+def change_address(product, address, tag):
+
+    command = f"python changeConfig.py --product {product} --path {address} --model patchcore  --tag {tag}"
+    print(command)
+    try:
+        subprocess.run(command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing the command: {e}")
+
+def inference(tag):
+    command = f"python tools/inference/lightning_inference.py --tag {tag}"
 
     try:
         subprocess.run(command, shell=True, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error executing the command: {e}")
+
+def get_path(option):
+
     option = option.split('_', 1)
     option = option[1]
-    path = f"results/patchcore/{option}/checkimages/check/"
+    path = f"datasets/{option}/check/"
     return path
+
+def get_product(option):
+    option = option.split('_', 1)
+    product = option[1]
+    return product
+
+def get_results(product, address):
+    tmp = address.split('/', -1)
+    folder = tmp[-2]
+    path = f"results/patchcore/{product}/checkimages/" + folder + "/"
+    return path
+
+
+@app.route('/upload_folder', methods=['POST'])
+def upload_folder():
+    uploaded_files = request.files.getlist('folder_path')
+    product = session.get('product')
+    if not os.path.exists("media/"+ product):
+        os.makedirs("media/"+ product)
+    
+    files = glob.glob("media/"+ product + "/*")    
+    for f in files:
+        os.remove(f)
+
+    for uploaded_file in uploaded_files:
+        if uploaded_file.filename != '':
+            # Define the destination folder on the server where you want to save the uploaded files
+            server_destination = 'media/'+ product + '/'
+
+            # Save each uploaded file to the server
+            destination_file = os.path.join(server_destination, uploaded_file.filename)
+            uploaded_file.save(destination_file)
+
+            # You can add additional processing for each uploaded file here
+    tag = session.get('tag')
+    product = session.get('product')
+    # Do something with the submitted address, e.g., process it or save it
+    address = 'media/' + product + '/'
+    change_address(product, address, tag)
+    results_path = get_results(product, address)
+    if os.path.exists(results_path):
+        files = glob.glob(results_path + "*")    
+        for f in files:
+            os.remove(f)
+    inference(tag)
+    
+    images = os.listdir(results_path)
+    # You can add a response indicating that all files were uploaded successfully
+    return render_template('thirdPage.html', address = address, images=images, tag=tag, results_path=results_path)
+
+
+
 
 @app.route('/<path:filename>')
 def serve_image(filename):  
-    # images_directory = os.path.join(os.getcwd(), 'media')
     images_directory = os.getcwd()
     return send_file(os.path.join(images_directory, filename))
 
